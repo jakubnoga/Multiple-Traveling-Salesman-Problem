604d7e2fe6c68ede25c798b0acf8388e416baa84
Jakub Noga <jnoga@student.agh.edu.pl>
2015-01-10 21:00:08 +0100

    minor changes to form and service

--- FRONT_END/app/scripts/services/main-service.js
+++ FRONT_END/app/scripts/services/main-service.js
@@ -10,8 +10,10 @@ angular.module('MTSPApp').service('MainService', function (
 		var url = '';
 		if(mode === 'map'){
 			url = 'mappoints';
+			data.tsplib = "null";
 		} else if (mode === 'tsplib') {
 			url = 'tsplib';
+			data.distanceMatrix = [[0]];
 		} else {
 			console.log('Unknown mode. STOP');
 			return;
@@ -19,7 +21,8 @@ angular.module('MTSPApp').service('MainService', function (
 		return $http({
 			method:'POST',
 			url: serverURL.url + url,
-			data: 'data=' + JSON.stringify(data)
+			data: 'data=' + JSON.stringify(data),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 		}).success(function(resolve){
 			window.alert('SUKCES, dane w konsoli');
 			console.log(resolve);
